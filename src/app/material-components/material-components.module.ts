8877eef9ac3922d6f248453fc4e1a5024a2487ea
Ess-Bidali <essiebidali@gmail.com>
2023-11-17 15:39:07 +0300

    Add angular material package for styling

---
+++ src/app/material-components/material-components.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatInputModule,
+    MatToolbarModule,
+  ]
+})
+export class MaterialComponentsModule { }
