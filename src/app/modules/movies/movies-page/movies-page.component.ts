7d099f6494d61a6dc37d0abfb6c127e87984f8bb
Ess-Bidali <essiebidali@gmail.com>
2023-11-17 17:59:54 +0300

    Add filter functionality to movies page

--- src/app/modules/movies/movies-page/movies-page.component.ts
+++ src/app/modules/movies/movies-page/movies-page.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { catchError, debounceTime, of, take } from 'rxjs';
+import { MoviesService } from 'src/app/state/movies/movies.service';
 
 @Component({
   selector: 'app-movies-page',
   templateUrl: './movies-page.component.html',
   styleUrls: ['./movies-page.component.scss']
 })
-export class MoviesPageComponent {
+export class MoviesPageComponent implements OnInit {
   searchControl = new FormControl<string>('');
+
+  constructor(private moviesService: MoviesService,
+    private _snackBar: MatSnackBar) {
+
+  }
+
+  ngOnInit() {
+    this.moviesService.get();
+    this.searchControl?.valueChanges.pipe(
+      debounceTime(500)
+    ).subscribe((searchTerm) => this.applyFilter(searchTerm));
+  }
+
+  applyFilter(searchTerm: string | null) {
+    if(searchTerm && searchTerm?.trim()?.length) {
+      this.moviesService.applyFilter(searchTerm)
+        .pipe(
+          take(1),
+          catchError((err) => {
+            this._snackBar.open('ERROR: ' + err.message, 'Close', {
+              verticalPosition: 'top',
+            });
+
+            return of([]);
+          })
+        )
+        .subscribe();
+    }
+
+  }
 }
