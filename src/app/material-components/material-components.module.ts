ef683dad95a79c6ea7b5c997827d7f2880e37a2f
Ess-Bidali <essiebidali@gmail.com>
2023-11-17 16:02:14 +0300

    Add more material components

--- src/app/material-components/material-components.module.ts
+++ src/app/material-components/material-components.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 const components = [
   CommonModule,
   MatInputModule,
   MatToolbarModule,
+  MatIconModule
 ];
 
 
