f5e6501dd670a35857aad2a3f61e127560a80625
Ess-Bidali <essiebidali@gmail.com>
2023-11-17 19:08:31 +0300

    Add error states

--- src/app/state/movies/movies.service.ts
+++ src/app/state/movies/movies.service.ts
@@ -16,30 +16,83 @@ export class MoviesService {
     this.store.set(testData);
   }
 
-  applyFilter(filter: string) {
+  /**
+   * Format the provided filters and fetch the new list of values
+   * @param filter
+   * @param page
+   * @param type
+   * @returns
+   */
+  applyFilter(filter: string, page: number = 1, type?: 'movies' | 'series') {
     const params = {
       s: filter,
+      page
     };
+    this.store.setLoading(true);
 
-    return from(this.performFetch(params))
+    return from(this._performFetch(params))
       .pipe(
-        tap((val) => console.log('val')),
-        tap((val) => {debugger}),
         map((data) => {
+          this.store.setLoading(false);
           if(data?.Search) {
             this.store.set(data?.Search)
           }
 
           if(data.Error) {
-            throw new Error(data.Error);
+            this.handleError(data.Error, params);
+          } else {
+            this.store.setError(null);
           }
         })
       )
   }
 
-  async performFetch(params: { [paramKey: string]: string }): Promise<MovieEndpointResponse> {
+  /**
+   * Handle errors thrown by API
+   * @param error The error object
+   * @param errorInput Input that led to the error
+   */
+  handleError(error: string, errorInput: any) {
+    console.log('Input causing error: ', errorInput);
+    this.store.set([]);
+
+    if(typeof error === 'string') {
+      switch(error?.toLowerCase()) {
+        case 'too many results.':
+          this.store.setError({ message: error});
+          break;
+        case 'movie not found!':
+          // Ignore errors
+          console.warn(error);
+          break;
+
+        default:
+          // Alert user
+          throw new Error(error);
+      }
+    } else {
+      console.log(error);
+
+      // Throw generic error
+      throw new Error('An error occurred!');
+    }
+  }
+
+  /**
+   * Reset the store to the initial value
+   */
+  resetStore() {
+    this.store.reset();
+  }
+
+  /**
+   * Make the actual API call, applying the required apiKey and any additional parameters
+   * @param params Query parameters to be appended to the request
+   * @returns
+   */
+  private async _performFetch(params: { [paramKey: string]: string | number | undefined | null }): Promise<MovieEndpointResponse> {
     const queryParams = new URLSearchParams({
-      token: environment.apiKey,
+      apiKey: environment.apiKey,
       ...params
     });
 
