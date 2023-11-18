b828b8ccc7f37092e0d94814536fa5c41fa90bb6
Ess-Bidali <essiebidali@gmail.com>
2023-11-18 14:55:07 +0300

    Add production file replacements

--- src/app/state/movies/movies.service.ts
+++ src/app/state/movies/movies.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { MoviesStore } from './movies.store';
 import { from, map, tap } from 'rxjs';
-import { environment } from '../../../environments/environment';
+import { environment } from 'src/environments/environment';
 import { MovieEndpointResponse, MovieQueryParams } from './movie.model';
 
 @Injectable({ providedIn: 'root' })
